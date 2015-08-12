dfeed94584c4370127a043e47b792b9238ce8373
Laurent GAY <l.gay@sd-libre.fr>
2015-08-12 10:03:21 +0200

    rename accounting -> financial

--- setup.py
+++ setup.py
@@ -59,5 +59,5 @@ setup(
     },
     install_requires=["lucterios >=2.0b0,<=2.0b999999999999999", \
                       "lucterios-contacts >=2.0b0,<=2.0b999999999999999", \
-                      "diacamma-accounting >=2.0b0,<=2.0b999999999999999"],
+                      "diacamma-financial >=2.0b0,<=2.0b999999999999999"],
 )
