40f3550355d2e9cfc47da6bc7d62b145405830f3
Laurent GAY <l.gay@sd-libre.fr>
2018-02-15 13:12:02 +0100

    add syndic test

---
+++ diacamma/syndic/tests.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+'''
+diacamma.syndic tests package
+
+@author: Laurent GAY
+@organization: sd-libre.fr
+@contact: info@sd-libre.fr
+@copyright: 2018 sd-libre.fr
+@license: This file is part of Lucterios.
+
+Lucterios is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lucterios is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lucterios.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from __future__ import unicode_literals
+from shutil import rmtree
+
+from django.utils import six
+
+from lucterios.framework.test import LucteriosTest, add_user
+from lucterios.framework.filetools import get_user_dir
+from lucterios.framework.xfergraphic import XferContainerAcknowledge
+from lucterios.contacts.models import Individual
+from lucterios.CORE.views import get_wizard_step_list
+
+from diacamma.accounting.test_tools import initial_thirds, default_compta, default_costaccounting
+from diacamma.payoff.test_tools import default_bankaccount, default_paymentmethod, PaymentTest
+from diacamma.condominium.test_tools import default_setowner, add_test_callfunds, old_accounting, add_test_expenses, init_compta, add_years
+
+
+class SyndicTest(PaymentTest):
+
+    def setUp(self):
+        self.xfer_class = XferContainerAcknowledge
+        initial_thirds()
+        LucteriosTest.setUp(self)
+        default_compta(with12=False)
+        default_bankaccount()
+        default_setowner()
+        contact = Individual.objects.get(id=5)
+        contact.user = add_user('joe')
+        contact.save()
+        rmtree(get_user_dir(), True)
+        add_years()
+        init_compta()
+        add_test_callfunds(False, True)
+        add_test_expenses(False, True)
+
+    def test_status(self):
+        self.calljson('/CORE/authentification', {'username': 'admin', 'password': 'admin'})
+        self.assert_observer('core.auth', 'CORE', 'authentification')
+        self.assert_json_equal('', '', 'OK')
+
+        self.calljson('/CORE/statusMenu', {})
+        self.assert_observer('core.custom', 'CORE', 'statusMenu')
+        self.assert_count_equal('', 16)
+
+    def test_wizard(self):
+        self.calljson('/CORE/authentification', {'username': 'admin', 'password': 'admin'})
+        self.assert_observer('core.auth', 'CORE', 'authentification')
+        self.assert_json_equal('', '', 'OK')
+
+        steplist = get_wizard_step_list()
+        self.assertEqual(17, len(steplist.split(';')), steplist)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 1})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 18)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 2})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 14)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 3})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 9)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 4})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 5})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 11)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 6})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 7})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 8})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 19)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 9})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 17)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 10})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 11})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 12})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 13})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 8)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 14})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 9)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 15})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 10)
+
+        self.calljson('/CORE/configurationWizard', {'steplist': steplist, 'step': 16})
+        self.assert_observer('core.custom', 'CORE', 'configurationWizard')
+        self.assert_count_equal('', 13)
+
+    def test_situation(self):
+        self.calljson('/CORE/authentification', {'username': 'joe', 'password': 'joe'})
+        self.assert_observer('core.auth', 'CORE', 'authentification')
+        self.assert_json_equal('', '', 'OK')
+
+        self.calljson('/CORE/situationMenu', {})
+        self.assert_observer('core.custom', 'CORE', 'situationMenu')
+        self.assert_count_equal('', 8)
